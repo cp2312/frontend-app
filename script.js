bb383bed5bf27b3562428ac6182290d562a58e9f
cp2312 <fernandopuli59@gmail.com>
2025-12-01 13:11:52 -0500

    Frontend listo

---
+++ script.js
@@ -0,0 +1,23 @@
+const API = "https://TU_BACKEND.onrender.com/api";
+
+async function cargarUsuarios() {
+  const res = await fetch(`${API}/users`);
+  const data = await res.json();
+
+  document.getElementById("listaUsuarios").innerHTML =
+    data.map(u => `<li>${u.name}</li>`).join("");
+}
+
+async function agregarUsuario() {
+  const name = document.getElementById("nameInput").value;
+
+  await fetch(`${API}/users`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name })
+  });
+
+  cargarUsuarios();
+}
+
+cargarUsuarios();
